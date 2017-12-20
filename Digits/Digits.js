69a8d79c87576370a45d7b8bb410634a3e134ce5
srinivasa keshavaiyer <srinivasa keshavaiyer>
2017-12-20 15:52:06 +0530

    Add project files.

---
+++ Digits/Digits.js
@@ -0,0 +1,13 @@
+﻿function checkDigits()
+{
+    var val = document.getElementById('val').value;
+
+    var digitCount = val.replace(/\D/g, '').length;
+
+    if (digitCount > 0) {
+        alert("Your value contains " + digitCount + " numbers");
+    }
+    else {
+        alert("Your value does not contain any number");
+    }
+}
---
+++ Palendrome/Palendrome.js
@@ -0,0 +1,15 @@
+﻿function checkPalendrome()
+{
+    var val = document.getElementById('val').value;
+
+    var reverseVal = val.split("").reverse().join("");
+
+    if (val.toUpperCase() == reverseVal.toUpperCase)
+    {
+        alert("Your value is a palendrome");
+    }
+    else
+    {
+        alert("Your value is not a palendrome");
+    }
+}
